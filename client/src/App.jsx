97f10d27163a62dd15d86176221414b279673026
Ali <aly2292005@gmail.com>
2023-08-12 13:24:01 +0300

    setup react redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
-import Home from './pages/Home'
-import NavBar from './components/NavBar'
+/* libs */ 
 import {
-  BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
   useLocation,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
+/* components */
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+
 
 function App() {
   const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
 
   console.log(location)
   return (
@@ -17,6 +21,7 @@ function App() {
       {location.pathname !== '/login' && location.pathname !== '/register' && <NavBar />}
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </main>
     // <h1>hey</h1>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store} >
+      <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <App />
+          </Router>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
