1ed8fb6ec70d5e326c68d92663e7f38dc8168045
Ali <aly2292005@gmail.com>
2023-08-17 13:47:54 +0300

    styling products page

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -1,9 +1,34 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import ProductCard from '../../components/ProductCard'
+import './Products.css'
 
 
-const Products = ({ searchQuery }) => {
+// for drawer 
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+// import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+
+import List from '@mui/material/List';
+
+import MenuIcon from '@mui/icons-material/Tune';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+const drawerWidth = 240;
+
+
+
+
+const Products = ({ searchQuery, props }) => {
+
+
+   // js
   const [products, setProducts] = useState([]);
   const [query, setQuery] = useState('popular');
   const [filters, setFilters] = useState({
@@ -39,21 +64,31 @@ const Products = ({ searchQuery }) => {
     }
   }, [filters, query, searchQuery]); //eslint-disable-line
 
-  return (
-    <div className="products">
-      {searchQuery 
-      ? <h1 style={{marginBlock: '50px'}}>search: <span style={{color: 'blue'}}>{searchQuery}</span></h1> 
-      : <h1 style={{marginBlock: '50px'}}>Products</h1>}
-      <div style={{margin: '50px'}} className="sorts">
-        <button onClick={()=> setQuery('new')} style={{backgroundColor: query==='new' && '#aaeeee'}}>newest</button>
-        <button onClick={()=> setQuery('old')} style={{backgroundColor: query==='old' && '#aaeeee'}}>oldest</button>
-        <button onClick={()=> setQuery('popular')} style={{backgroundColor: query==='popular' && '#aaeeee'}}>most popular</button>
-        <button onClick={()=> setQuery('sold')} style={{backgroundColor: query==='sold' && '#aaeeee'}}>most selled</button>
-      </div>
-      <div className="products-part" style={{display: 'flex'}}>
-        <div className="filters" style={{width: '25%'}}>
+// for class name in filters 
+
+
+
+
+
+// for drawer 
+const { window } = props || {}; 
+const [mobileOpen, setMobileOpen] = useState(false);
+
+const handleDrawerToggle = () => {
+  setMobileOpen(!mobileOpen);
+};
+
+
+const drawer = (
+  < >
+    <Toolbar />
+    <Divider />
+    <List sx={{top:" 65.6px"}}>
+
+    <div className="products-part" style={{display: 'flex'}}>
+        <div className="filters" >
           <div className="type-filter-block">
-            <h3>product type</h3>
+            <h3 className="h3-drawer">product type</h3>
             <ul>
               {['tshirt', 'pants', 'shoes'].map(type => (
                 <li 
@@ -73,7 +108,7 @@ const Products = ({ searchQuery }) => {
             </ul>
           </div>
           <div style={{marginTop: '30px'}} className="gender-filter-block">
-            <h3>gender</h3>
+            <h3 className="h3-drawer">gender</h3>
             <ul>
               {['male', 'female'].map(gender => (
                 <li 
@@ -93,10 +128,11 @@ const Products = ({ searchQuery }) => {
             </ul>
           </div>
           <div style={{marginTop: '30px'}} className="sizes-filter-block">
-            <h3>sizes</h3>
-            <ul style={{display: 'flex', flexWrap: 'wrap'}}>
+            <h3 className="h3-drawer">sizes</h3>
+            <ul style={{display: 'flex', flexDirection: 'column', margin: '18px', gap: '11px'}}>
               {['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].map(size => (
                 <button 
+                className={`sizes-button animated-button`}
                 key={size} 
                 onClick={()=> setFilters(prev => {
                   return {
@@ -105,16 +141,134 @@ const Products = ({ searchQuery }) => {
                     ? prev.sizes.filter((s) => s !== size)
                     : [...prev.sizes, size]
                   }
-                })}
+                }) }
                 style={{
-                  marginInline: '5px', 
-                  backgroundColor: filters.sizes.includes(size) && '#aaeeee'
+                  boxShadow: filters.sizes.includes(size) && " 0 0 0 5px rgba(0, 0, 0, 0.2)",
+                  color: filters.sizes.includes(size) && "#ffffff",
+                  background: filters.sizes.includes(size) && "#232323",
                 }}>{size}</button>
               ))}
             </ul>
           </div>
         </div>
-        <div className="cards" style={{width: '70%', display: 'flex', maxWidth: '70%', flexWrap: 'wrap',}}>
+        </div>
+
+    </List>
+  </>
+);
+
+const container = window !== undefined ? () => window().document.body : undefined;
+
+
+
+
+
+
+  return (
+    <div className="products">
+
+
+
+<Box sx={{ display: 'flex' }}>
+
+      {/* <CssBaseline /> */}
+
+{/* the drawer nav bar */}
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          zIndex: '1',
+          paddingTop: '66.6px',
+          boxShadow: 'none',
+          backgroundColor: '#fff',
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="#232323"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" className="drawer-navbar">
+          {searchQuery 
+          ? <h1 >search: <span style={{color: 'blue'}}>{searchQuery}</span></h1> 
+          : <h1 className="h1-drawer-navbar">Products</h1>}
+          <br/>
+          <div  className="sorts">
+          <button onClick={()=> setQuery('new') }  className={`${query === "new" && "active1"} animated-button` }>newest</button>
+          <button onClick={()=> setQuery('old')}  className={`${query === "old" && "active1"} animated-button` }>oldest</button>
+          <button onClick={()=> setQuery('popular')}  className={`${query === "popular" && "active1"} animated-button` }>most popular</button>
+          <button onClick={()=> setQuery('sold')}  className={`${query === "sold" && "active1"} animated-button` }>most selled</button>
+          </div>
+          </Typography> 
+        </Toolbar>
+        <hr className="MuiDivider-root MuiDivider-fullWidth css-9mgopn-MuiDivider-root"/>
+      </AppBar>
+
+
+
+
+
+{/* the drawer  */}
+      <Box
+        className="drawer"
+        zIndex="-1"
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+
+
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          className="drawer"
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+
+
+
+      <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+          className="drawer"
+        >
+          {drawer}
+        </Drawer>
+
+
+
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+
+
+
+        <div className="cards" style={{width: '100%', display: 'flex', maxWidth: '100%', flexWrap: 'wrap',}}>
         {products.length < 1 
           ? (<h2>no products</h2>)
           : products.map(p => (
@@ -122,9 +276,193 @@ const Products = ({ searchQuery }) => {
           ))
         }
         </div>
-      </div>
+
+
+
+      </Box>
+    </Box>
+
+
+
     </div>
   )
 }
 
+
+
 export default Products
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// why the first code filters is not working
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+// import { useEffect, useState } from "react"
+// import ProductCard from '../../components/ProductCard'
+
+
+// const Products = ({ searchQuery }) => {
+//   const [products, setProducts] = useState([]);
+//   const [query, setQuery] = useState('popular');
+//   const [filters, setFilters] = useState({
+//     type: '',
+//     gender: '',
+//     sizes: [],
+//   });
+//   const API_LINK = import.meta.env.VITE_API_KEY
+
+//   useEffect(() => {
+//     if (query) {
+//       axios.get(`${API_LINK}/products`, {
+//         params: {
+//           [query]: true,
+//           ...filters,
+//           'search': searchQuery,
+//         }
+//       })
+//       .then(res => {
+//         setProducts(res.data)
+//       })
+//       .catch(err => {
+//         console.log(`error fetching products with params "${query}:true"`, err)
+//       })
+//     }else {
+//       axios.get(`${API_LINK}/products`)
+//       .then(res => {
+//         setProducts(res.data)
+//       })
+//       .catch(err => {
+//         console.log('error fetching recent added products', err)
+//       })
+//     }
+//   }, [filters, query, searchQuery]); //eslint-disable-line
+
+//   return (
+//     <div className="products">
+//       {searchQuery 
+//       ? <h1 style={{marginBlock: '50px'}}>search: <span style={{color: 'blue'}}>{searchQuery}</span></h1> 
+//       : <h1 style={{marginBlock: '50px'}}>Products</h1>}
+//       <div style={{margin: '50px'}} className="sorts">
+//         <button onClick={()=> setQuery('new')} style={{backgroundColor: query==='new' && '#aaeeee'}}>newest</button>
+//         <button onClick={()=> setQuery('old')} style={{backgroundColor: query==='old' && '#aaeeee'}}>oldest</button>
+//         <button onClick={()=> setQuery('popular')} style={{backgroundColor: query==='popular' && '#aaeeee'}}>most popular</button>
+//         <button onClick={()=> setQuery('sold')} style={{backgroundColor: query==='sold' && '#aaeeee'}}>most selled</button>
+//       </div>
+//       <div className="products-part" style={{display: 'flex'}}>
+//         <div className="filters" style={{width: '25%'}}>
+//           <div className="type-filter-block">
+//             <h3>product type</h3>
+//             <ul>
+//               {['tshirt', 'pants', 'shoes'].map(type => (
+//                 <li 
+//                 key={type}
+//                 onClick={()=> setFilters(prev => {
+//                   return {
+//                     ...prev, 
+//                     type: prev.type===type ? '' : type,
+//                   }
+//                 })}
+//                 style={{
+//                   margin: '15px',
+//                   cursor: 'pointer',
+//                   color: filters.type === type && 'blue',
+//                 }}>{type}</li>
+//               ))}
+//             </ul>
+//           </div>
+//           <div style={{marginTop: '30px'}} className="gender-filter-block">
+//             <h3>gender</h3>
+//             <ul>
+//               {['male', 'female'].map(gender => (
+//                 <li 
+//                 key={gender}
+//                 onClick={()=> setFilters(prev => {
+//                   return {
+//                     ...prev,
+//                     gender: prev.gender===gender ? '' : gender,
+//                   }
+//                 })}
+//                 style={{
+//                   margin: '15px',
+//                   cursor: 'pointer',
+//                   color: filters.gender === gender && 'blue',
+//                 }}>{gender}</li>
+//               ))}
+//             </ul>
+//           </div>
+//           <div style={{marginTop: '30px'}} className="sizes-filter-block">
+//             <h3>sizes</h3>
+//             <ul style={{display: 'flex', flexWrap: 'wrap'}}>
+//               {['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].map(size => (
+//                 <button 
+//                 key={size} 
+//                 onClick={()=> setFilters(prev => {
+//                   return {
+//                     ...prev, 
+//                     sizes: prev.sizes.includes(size) 
+//                     ? prev.sizes.filter((s) => s !== size)
+//                     : [...prev.sizes, size]
+//                   }
+//                 })}
+//                 style={{
+//                   marginInline: '5px', 
+//                   backgroundColor: filters.sizes.includes(size) && '#aaeeee'
+//                 }}>{size}</button>
+//               ))}
+//             </ul>
+//           </div>
+//         </div>
+//         <div className="cards" style={{width: '70%', display: 'flex', maxWidth: '70%', flexWrap: 'wrap',}}>
+//         {products.length < 1 
+//           ? (<h2>no products</h2>)
+//           : products.map(p => (
+//             <ProductCard sizes={p.sizes} gender={p.gender} views={p.views} sold={p.sold} title={p.title} createdAt={p.createdAt} key={p._id} desc={p.desc} price={p.price} />
+//           ))
+//         }
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Products
+
+
+
+
+
+
+
+
